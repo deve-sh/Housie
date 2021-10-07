d1d292b46923ec619a04ce61ef2cbaab286e6d01
Devesh Kumar <devesh2027@gmail.com>
2021-10-07 12:16:12 +0530

    Setup firebase authentication

---
+++ src/firebase/authentication.js
@@ -0,0 +1,14 @@
+// File to house the global authentication ref of firebase.
+
+import firebase from "./index";
+import mainFirebase from "firebase/app";
+import "firebase/auth";
+
+const auth = firebase.auth();
+
+// Providers
+const googleProvider = new mainFirebase.auth.GoogleAuthProvider();
+const githubProvider = new mainFirebase.auth.GithubAuthProvider();
+
+export default auth;
+export const providers = { googleProvider, githubProvider, mainFirebase };
