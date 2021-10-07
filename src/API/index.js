4213fe775ee4bb8ae96e3d2029bd7a45aad8d64c
Devesh Kumar <devesh2027@gmail.com>
2021-10-07 12:19:01 +0530

    Added function to create a housie game

---
+++ src/API/index.js
@@ -0,0 +1,28 @@
+import auth from "../firebase/authentication";
+import db, { firestore } from "../firebase/db";
+
+export const createHousieGame = (maxPlayers, callback) => {
+	try {
+		if (!auth.currentUser)
+			return callback("You need to be signed in to create a game");
+
+		const gameRef = db.collection("games").doc();
+
+		const gameData = {
+			gameId: gameRef.id,
+			createdBy: auth.currentUser.uid,
+			createdAt: firestore.FieldValue.serverTimestamp(),
+			updatedAt: firestore.FieldValue.serverTimestamp(),
+			started: false,
+			startedAt: null,
+			numbersDrawn: [],
+			maxPlayers,
+		};
+
+		await gameRef.set(gameData);
+		return callback(null, gameData);
+	} catch (err) {
+		console.log(err);
+		return callback(err.message, null);
+	}
+};
