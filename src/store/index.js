881f8acb41f5cf83fa129ccdeff53359be3148bd
Devesh Kumar <devesh2027@gmail.com>
2021-10-09 14:40:03 +0530

    Added global store with local storage persistence

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { writable } from "svelte/store";
+
+// Get persisted value from local storage.
+const storedStore = JSON.parse(
+	localStorage.getItem("housie-store") || '{ "user": null }'
+);
+
+const store = writable(storedStore);
+
+// Persist changes to local storage.
+theme.subscribe((value) =>
+	localStorage.setItem("housie-store", JSON.stringify(value))
+);
+
+export default store;
