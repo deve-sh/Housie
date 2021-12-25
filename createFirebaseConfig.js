be4b72a09140a1de2a8e91839c2e03325983b9ca
Devesh Kumar <devesh2027@gmail.com>
2021-12-25 09:23:55 +0530

    Updated createFirebaseConfig.js file

--- createFirebaseConfig.js
+++ createFirebaseConfig.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 
 const createFirebaseConfig = () => {
 	console.log("Generating Firebase Config File");
-	const fileContents = `const firebaseConfig = ${process.env.FIREBASE_CONFIG};
+	if (process.env.FIREBASE_CONFIG) {
+		const fileContents = `const firebaseConfig = ${process.env.FIREBASE_CONFIG};
 export default firebaseConfig;`;
-	fs.writeFileSync("./src/firebase/config.js", fileContents);
-	console.log("Generated Firebase Config File");
+		fs.writeFileSync("./src/firebase/config.js", fileContents);
+		console.log("Generated Firebase Config File");
+	}
 };
 
 createFirebaseConfig();
